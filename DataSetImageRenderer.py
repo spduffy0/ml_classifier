bfaaea791de3e2d2450950ed1a35853407fe7334
Sean Duffy <spduffy0@gmail.com>
2025-03-30 12:59:17 -0500

    change function name to match syntax

--- DataSetImageRenderer.py
+++ DataSetImageRenderer.py
@@ -1,23 +1,23 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import torch 
 import torchvision as tvision
 from torch.utils.data import DataLoader
 
-# functions to show an image
-
-def imshow(img):
-    img = img / 2 + 0.5     # unnormalize
+# Render Images
+def display_images(img):
+    # Unnormalize image data
+    img = img / 2 + 0.5
     npimg = img.numpy()
     plt.imshow(np.transpose(npimg, (1, 2, 0)))
     plt.show()
 
-def showexample(trainloader: DataLoader, classes):
+# Show an example of the training Images
+def show_example(trainloader: DataLoader, classes):
     # get some random training images
-    dataiter = iter(trainloader)
-    images, labels = next(dataiter)
+    data_iter = iter(trainloader)
+    images, labels = next(data_iter)
 
     # show images
-    imshow(tvision.utils.make_grid(images))
+    display_images(tvision.utils.make_grid(images))
     # print labels
     print(' '.join(f'{classes[labels[j]]:5s}' for j in range(4)))
